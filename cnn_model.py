f0300360739ea7150b0dca4848e798bcdf38655d
Mukul Sharma <mukulsharma.ajm1997@gmail.com>
2020-05-26 17:41:07 +0530

    first commit

---
+++ cnn_model.py
@@ -0,0 +1,94 @@
+#inporting all the libraries
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.layers import Activation
+from keras.optimizers import RMSprop
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.preprocessing.image import ImageDataGenerator
+
+model = Sequential()
+
+# making cnn layers
+model.add(Convolution2D(filters=32, 
+                        kernel_size=(3,3), 
+                        activation='relu',
+                   input_shape=(200,200 , 3)
+                       ))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(units=128, activation='relu'))
+model.add(Dense(units=2, activation='sigmoid'))
+
+#model.summary()
+
+# load data for train a model
+train_data_dir = 'C:/my/linuxworld_programs/mlops/ml-devops/dataset/training_set/'
+validation_data_dir = 'C:/my/linuxworld_programs/mlops/ml-devops/dataset/test_set/'
+
+img_rows = 200
+img_cols = 200 
+train_datagen = ImageDataGenerator(
+      rescale=1./255,
+      rotation_range=20,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
+ 
+validation_datagen = ImageDataGenerator(rescale=1./255)
+ 
+# Change the batchsize according to your system RAM
+train_batchsize = 16
+val_batchsize = 10
+ 
+train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=train_batchsize,
+        class_mode='categorical')
+ 
+validation_generator = validation_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=val_batchsize,
+        class_mode='categorical',
+        shuffle=False)
+
+# start training a model
+checkpoint = ModelCheckpoint("cnn_model.h5",
+                             monitor="val_loss",
+                             mode="min",
+                             save_best_only = True,
+                             verbose=1)
+
+earlystop = EarlyStopping(monitor = 'val_loss', 
+                          min_delta = 0, 
+                          patience = 3,
+                          verbose = 1,
+                          restore_best_weights = True)
+
+# we put our call backs into a callback list
+callbacks = [earlystop, checkpoint]
+
+# Note we use a very small learning rate 
+model.compile(loss = 'categorical_crossentropy',
+              optimizer = RMSprop(lr = 0.001),
+              metrics = ['accuracy'])
+
+nb_train_samples = 1190
+nb_validation_samples = 170
+epochs = 5
+batch_size = 16
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch = nb_train_samples // batch_size,
+    epochs = epochs,
+    callbacks = callbacks,
+    validation_data = validation_generator,
+    validation_steps = nb_validation_samples // batch_size)
+
+model.save("cnn_model.h5")
